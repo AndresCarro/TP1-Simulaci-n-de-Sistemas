cc298dc0104a8d2f5ba6710a88a814cee76554a0
Gaston Francois <gfrancois@itba.edu.ar>
2024-03-10 13:08:09 -0300

    valores de input para dar vecinos

--- TP1/src/main/java/Main.java
+++ TP1/src/main/java/Main.java
@@ -4,20 +4,14 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        Gson gson = new Gson();
-        SimulationConfig sConfig = null;
-        try (FileReader reader = new FileReader("input.json")) {
-            sConfig = gson.fromJson(reader, SimulationConfig.class);
-            System.out.println(sConfig);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // TODO: Leer de un archivo
+        int M = 10;
+        double L = 30;
+        double radius = 9.8F;
+        int N = 1000;
+        double radiusNeighbour = 3;
 
-        if(sConfig == null){
-            return;
-        }
-
-        SimulationFactory simulator = new SimulationFactory(sConfig.getM(), sConfig.getL(), sConfig.getN(), sConfig.getRadius(), sConfig.getRadiusNeighbour());
+        SimulationFactory simulator = new SimulationFactory(M, L, N, radius, radiusNeighbour);
 
         // TEST para saber si esta bien
         simulator.printParticles();
